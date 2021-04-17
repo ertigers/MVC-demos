b1ac2c3ef0ebd0eb5564555cd4e8f3604f9d37cb
yihu-lv <1720427782@qq.com>
2021-04-17 12:10:51 +0800

    mvc思想实现

--- src/app2/app2.js
+++ src/app2/app2.js
@@ -1,26 +1,27 @@
-import "./app2.css";
-import $ from "jquery";
+import './app2.css'
+import Vue from 'vue'
 
-const $tabBar = $("#app2 .tab-bar");
-const $tabContent = $("#app2 .tab-content");
+const init = (el) => {
+  new Vue({
+    el: el,
+    data: {
+      index: 0
+    },
+    template: `
+      <section id="app2">
+        <ol class="tab-bar">
+          <li :class="index === 0 ? 'selected' : ''"
+            @click="index=0"><span>1111</span></li>
+          <li :class="index === 1 ? 'selected' : ''"
+            @click="index=1"><span>2222</span></li>
+        </ol>
+        <ol class="tab-content">
+          <li :class="index === 0 ? 'active': ''" >内容1</li>
+          <li :class="index === 1? 'active': ''" >内容2</li>
+        </ol>
+      </section>
+    `
+  })
+}
 
-const index = localStorage.getItem("app2.index") || 0
-
-
-$tabBar.on("click", "li", e => {
-  const $li = $(e.currentTarget);
-  $li
-    .addClass("selected")
-    .siblings()
-    .removeClass("selected");
-  const index = $li.index();
-  localStorage.setItem("app2.index",index)
-  $tabContent
-    .children()
-    .eq(index)
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
-});
-
-$tabBar.children().eq(index).trigger('click')
+export default init
--- src/app3/app3.js
+++ src/app3/app3.js
@@ -1,26 +1,25 @@
 import $ from 'jquery'
 import './app3.css'
+
+const html = `
+    <section id="app3">
+      <div class="square"></div>
+    </section>
+`
+const $element = $(html).appendTo($('body>.page'))
 const $square = $('#app3 .square')
+const localKey = 'app3.active'
+// yes no undefined
+const active = localStorage.getItem(localKey) === 'yes'
 
-const active = localStorage.getItem("app3.active") === "yes"
+$square.toggleClass('active', active)
 
-// if(active) {
-//   $square.addClass('active')
-// }else {
-//   $square.removeClass('active')
-// }
-
-$square.toggleClass('active',active)
-
-
-
-$square.on('click', ()=>{
-  if($square.hasClass('active')) {
+$square.on('click', () => {
+  if ($square.hasClass('active')) {
     $square.removeClass('active')
-    localStorage.setItem("app3.active","no")
-  }else {
+    localStorage.setItem(localKey, 'no')
+  } else {
     $square.addClass('active')
-    localStorage.setItem("app3.active","yes")
+    localStorage.setItem('app3.active', 'yes')
   }
-  // $square.toggleClass('active')
 })
